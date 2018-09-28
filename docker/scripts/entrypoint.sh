44d61e36b9c45e959c85320a4ecc216080c09475
Kristijan Conkas <chonky@chonky.eu>
2018-09-28 22:44:53 +0100

    Generating Listeners.pm

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -5,6 +5,17 @@
 SPIDER_INSTALL_DIR=${SPIDER_INSTALL_DIR:-/spider}
 
 # generate config file using parameters passed via environment variables
+cat << EOF > ${SPIDER_INSTALL_DIR}/local/Listeners.pm
+package main;
+
+use vars qw(@listen);
+
+@listen = (
+    ["0.0.0.0", ${CLUSTER_PORT:-7300}],
+);
+
+1;
+EOF
 sed -e 's/\(\$mycall[[:space:]]*=[[:space:]]*\).*$/\1"'${CLUSTER_CALLSIGN}'";/' \
     -e 's/\(\$myname[[:space:]]*=[[:space:]]*\).*$/\1"'"${CLUSTER_SYSOP_NAME}"'";/' \
     -e 's/\(\$myalias[[:space:]]*=[[:space:]]*\).*$/\1"'${CLUSTER_SYSOP_CALLSIGN}'";/' \
@@ -13,8 +24,6 @@ sed -e 's/\(\$mycall[[:space:]]*=[[:space:]]*\).*$/\1"'${CLUSTER_CALLSIGN}'";/' 
     -e 's/\(\$mylocator[[:space:]]*=[[:space:]]*\).*$/\1"'$(echo ${CLUSTER_LOCATOR} | tr '[a-z]' '[A-Z]')'";/' \
     -e 's/\(\$myemail[[:space:]]*=[[:space:]]*\).*$/\1"'$(echo ${CLUSTER_SYSOP_EMAIL} | sed 's/\@/\\@/g')'";/' \
     -e 's/\(\$mybbsaddr[[:space:]]*=[[:space:]]*\).*$/\1"'$(echo ${CLUSTER_SYSOP_BBS_ADDRESS} | sed 's/\@/\\@/g')'";/' \
-    -e 's/\(\$clusteraddr[[:space:]]*=[[:space:]]*\).*$/\1"'$(hostname)'";/' \
-    -e 's/\(\$clusterport[[:space:]]*=[[:space:]]*\).*$/\1"'${CLUSTER_PORT:-27754}'";/' \
     < ${SPIDER_INSTALL_DIR}/perl/DXVars.pm.issue > ${SPIDER_INSTALL_DIR}/local/DXVars.pm && \
 cd ${SPIDER_INSTALL_DIR}/perl && \
 ./create_sysop.pl && \
